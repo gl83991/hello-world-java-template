e0c7ff24b40b5f1388ff348821781bf7dc101a83
Liam Galka <gl83991@mail.harper.college>
2025-08-24 22:22:16 -0500

    Assignment Complete

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-      Greeter greeter = new Greeter();
-      System.out.println(greeter.getGreeting());
+      System.out.println("Hello, World!");
     }
 }
